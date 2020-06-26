ef9a37b7a5d3e3ef712f4bacc1a19f77fb64469d
dilpreet kaur <dilpreet.kukreja@gmail.com>
2020-06-26 04:44:30 -0700

    second commit

--- controllers/exercise-controller.js
+++ controllers/exercise-controller.js
@@ -12,8 +12,6 @@ exports.addExercise = (req, res, next) => {
   let exercise;
   const hex = /[0-9A-Fa-f]{6}/g;
 
-  console.log(new Date(req.body.date).toUTCString());
-
   if (description == '') {
     return res.send('Path "description" is required.');
   }
@@ -31,19 +29,20 @@ exports.addExercise = (req, res, next) => {
     );
   } else {
     if (date) {
+      let output;
       if (isNaN(date)) {
         let datetemp = new Date(date);
         if (datetemp == 'Invalid Date') {
           return res.send('Enter Valid date');
         } else {
-          output = datetemp.getTime();
+          output = new Date(datetemp.getTime()).toUTCString();
         }
       } else {
         datetemp = new Date(+date);
         if (datetemp == 'Invalid Date') {
           return res.send('Enter Valid date');
         } else {
-          output = +datetemp;
+          output = new Date(+datetemp).toUTCString();
         }
       }
 
