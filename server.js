1836b7055cf694def8c03cf91fc6150c0520b3f0
dilpreet kaur <dilpreet.kukreja@gmail.com>
2020-06-26 02:12:01 -0700

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-require('dotenv').config();
+//require('dotenv').config();
 
 const cors = require('cors');
 
